2233f8997238438356dcf612241ed0bb342582ff
onotsky <zeidler.michael@gmail.com>
2026-07-05 19:14:16 +0200

    Version 5.0.0.1

--- ApfelmusFramework/Properties/AssemblyInfo.cs
+++ ApfelmusFramework/Properties/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Runtime.Versioning;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.1")]
+[assembly: AssemblyFileVersion("5.0.0.1")]
